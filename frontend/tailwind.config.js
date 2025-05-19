8cef0eaab313ad34188837e08c5369ad38eb2e69
황호선 <3262398@naver.com>
2025-05-19 22:10:18 +0900

    fix: 충돌 해결

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,11 +2,15 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+<<<<<<< HEAD
     extend: {
       colors: {
         deepnavy: "#14244C",
       },
     },
+=======
+    extend: {},
+>>>>>>> ff716a6 (fix: 백엔드 파일이 포함된 이전 커밋 되돌림)
   },
   plugins: [],
 };
